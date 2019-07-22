20ba38b0fa7fb1afac9b240d932bce2892780c7c
rualt <rualnar@gmail.com>
2019-07-22 22:21:14 +0400

    create Calc game

--- src/Games/calc.php
+++ src/Games/calc.php
@@ -2,42 +2,27 @@
 
 namespace BrainGames\Calc;
 
-use function \cli\line;
-use function \cli\prompt;
+use function \BrainGames\Engine\startGame;
 
-function isEven($number)
+function runGame()
 {
-    return $number % 2 === 0 ? 'yes' : 'no';
-}
+    $description = "What is the result of the expression?\n";
 
-function sayYes()
-{
-    line("Correct!");
-}
-
-function sayNo($ansver, $number)
-{
-    $correctAnsver = isEven($number);
-    line("'{$ansver}' is wrong ansver ;(. Correct answer was '{$correctAnsver}'.");
-}
-
-function run()
-{
-    line("Welcome to the Brain Games!");
-    line("Answer \"yes\" if number even otherwise answer \"no\".\n");
-    $name = prompt('May I have your name?');
-    line("Hello, $name!\n");
-
-    for ($i = 0; $i < 3;) {
-        $number = rand(0, 100);
-        line("Question: $number");
-        $ansver = prompt('Your answer');
-        if (isEven($number) === $ansver) {
-            sayYes();
-            $i++;
+    
+    $correctAnswer = 0;
+    $gameData = function () {
+        [$num1, $num2] = [rand(1, 10), rand(1, 10)];
+        $operators = ['+', '-', '*'];
+        $operator = $operators[rand(0, 2)];
+        $question = "$num1 $operator $num2";
+        if ($operator == '+') {
+            $correctAnswer = $num1 + $num2;
+        } elseif ($operator == '-') {
+            $correctAnswer = $num1 - $num2;
         } else {
-            sayNo($ansver, $number);
+            $correctAnswer = $num1 * $num2;
         }
-    }
-    line("\nCongratulations, $name!");
+        return ["$question", "$correctAnswer"];
+    };
+    startGame($description, $gameData);
 }
