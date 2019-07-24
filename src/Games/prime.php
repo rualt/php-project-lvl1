eeae1b82a0464194f55b0730409cff4f2b770282
rualt <rualnar@gmail.com>
2019-07-24 13:24:20 +0400

    fix range of random numbers

--- src/Games/prime.php
+++ src/Games/prime.php
@@ -18,7 +18,7 @@ function runGame()
 {
     $description = "Answer \"yes\" if given number is prime, otherwise answer \"no\".\n";
     $gameData = function () {
-        $question = rand(0, 100);
+        $question = rand(2, 100);
         $correctAnswer = isPrime($question) ? 'yes' : 'no';
         return [$question, $correctAnswer];
     };
