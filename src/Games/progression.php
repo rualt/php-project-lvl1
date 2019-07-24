85179232d57f00f12926b452401d3e9e164d8008
rualt <rualnar@gmail.com>
2019-07-24 12:02:25 +0400

    change progression diff to random

--- src/Games/progression.php
+++ src/Games/progression.php
@@ -7,8 +7,9 @@ use function \BrainGames\Engine\startGame;
 function makeRandProgression()
 {
     $progression = [rand(0, 80)];
+    $difference = rand(2, 4);
     for ($i = 0; $i < 10; $i++) {
-        $progression[] = $progression[$i] + 2;
+        $progression[] = $progression[$i] + $difference;
     }
     return $progression;
 }
