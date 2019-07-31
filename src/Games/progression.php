a64abab6a4d7128111a27ba4b58bf308b5187cd4
rualt <rualnar@gmail.com>
2019-07-31 16:52:56 +0400

    purge the unclean function

--- src/Games/progression.php
+++ src/Games/progression.php
@@ -4,11 +4,11 @@ namespace BrainGames\Progression;
 
 use function \BrainGames\Engine\runGame;
 
-function makeRandProgression()
+function makeProgression($firstNum, $difference)
 {
-    $progression = [rand(0, 80)];
-    $difference = rand(2, 4);
-    for ($i = 0; $i < 10; $i++) {
+    $progressionLenght = 10;
+    $progression[] = $firstNum;
+    for ($i = 0; $i < $progressionLenght - 1; $i++) {
         $progression[] = $progression[$i] + $difference;
     }
     return $progression;
@@ -18,12 +18,13 @@ function calcGame()
 {
     $description = 'What number is missing in the progression?';
     $getGameData = function () {
-        $progression = makeRandProgression();
-        $randKey = rand(0, 9);
+        $progression = makeProgression(rand(0, 80), rand(2, 4));
+        $progressionLenght = count($progression);
+        $randKey = rand(0, $progressionLenght - 1);
         $correctAnswer = $progression[$randKey];
         $progression[$randKey] = '..';
         $question = implode(' ', $progression);
-        return ["$question", "$correctAnswer"];
+        return [$question, "$correctAnswer"];
     };
     runGame($description, $getGameData);
 }
