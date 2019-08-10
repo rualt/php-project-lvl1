63961b3ef730cb764b1a2c248580ad1c2f737ab7
rualt <rualnar@gmail.com>
2019-08-10 19:25:00 +0400

    move $progressionLenght to const

--- src/Games/progression.php
+++ src/Games/progression.php
@@ -4,13 +4,13 @@ namespace BrainGames\Progression;
 
 use function \BrainGames\Engine\runGame;
 
+const PROGRESSION_LENGTH = 10;
 const DESCRIPTION = 'What number is missing in the progression?';
 
 function makeProgression($firstNum, $difference)
 {
-    $progressionLength = 10;
     $progression[] = $firstNum;
-    for ($i = 0; $i < $progressionLength - 1; $i++) {
+    for ($i = 0; $i < PROGRESSION_LENGTH - 1; $i++) {
         $progression[] = $progression[$i] + $difference;
     }
     return $progression;
@@ -20,8 +20,7 @@ function calcGame()
 {
     $getGameData = function () {
         $progression = makeProgression(rand(0, 80), rand(2, 4));
-        $progressionLength = count($progression);
-        $randKey = rand(0, $progressionLength - 1);
+        $randKey = rand(0, count($progression) - 1);
         $correctAnswer = $progression[$randKey];
         $progression[$randKey] = '..';
         $question = implode(' ', $progression);
