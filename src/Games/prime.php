289e6f942004f79ec949c7f88de95f4d19616bbe
rualt <rualnar@gmail.com>
2019-08-11 14:01:58 +0400

    change isPrime for second expression

--- src/Games/prime.php
+++ src/Games/prime.php
@@ -11,7 +11,7 @@ function isPrime($number)
     if ($number < 2) {
         return false;
     }
-    for ($i = 2; pow($i, 2) <= $number; $i++) {
+    for ($i = 2; $i <= sqrt($number); $i++) {
         if ($number % $i === 0) {
             return false;
         }
