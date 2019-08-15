be1a9131860f411853aea48ae44b27b93aac1a19
rualt <rualnar@gmail.com>
2019-08-15 12:00:37 +0400

    change calcGcd to getGcd

--- src/Games/gcd.php
+++ src/Games/gcd.php
@@ -6,12 +6,12 @@ use function \BrainGames\Engine\runGame;
 
 const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
 
-function calcGcd($num1, $num2)
+function getGcd($num1, $num2)
 {
     $biggerNum = $num1 > $num2 ? $num1 : $num2;
     $smallerNum = $num1 > $num2 ? $num2 : $num1;
     $remainder = $biggerNum % $smallerNum;
-    return $remainder == 0 ? $smallerNum : calcGcd($smallerNum, $remainder);
+    return $remainder == 0 ? $smallerNum : getGcd($smallerNum, $remainder);
 }
 
 function calcGame()
@@ -20,7 +20,7 @@ function calcGame()
         $num1 = rand(1, 100);
         $num2 = rand(1, 100);
         $question = "$num1 $num2";
-        $correctAnswer = calcGcd($num1, $num2);
+        $correctAnswer = getGcd($num1, $num2);
         return [$question, (string) $correctAnswer];
     };
     runGame(DESCRIPTION, $getGameData);
